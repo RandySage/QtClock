9d2db53fca01375056d46b44f89ba78a3a37259a
Randy Sage <rsage@thinhorn.com>
2024-01-19 15:48:20 -0800

    Example code given by ChatGPT

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Setup the timer
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MainWindow::updateTime);
+    timer->start(100); // Update every 100 ms
+
+    updateTime(); // Initial time update
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +19,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::updateTime()
+{
+    QTime time = QTime::currentTime();
+    QString timeText = time.toString("hh:mm:ss.zzz");
+    ui->timeLabel->setText(timeText);
+}
